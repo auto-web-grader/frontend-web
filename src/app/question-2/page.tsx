789270c7f06f781b47cee7b9544c86af75393848
shafa <132644198+shafakirrn@users.noreply.github.com>
2024-09-28 23:51:19 +0700

    feat: update judul soal 1&2

--- src/app/question-2/page.tsx
+++ src/app/question-2/page.tsx
@@ -204,7 +204,7 @@ export default function Question2() {
                   weight='bold'
                   className='text-lg md:text-xl lg:text-2xl' // Adjusting text size
                 >
-                  Submission Question 2
+                  Analisis Data
                 </Typography>
               </div>
             </div>
